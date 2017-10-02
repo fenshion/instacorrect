8785a53d8ce14db135a43065e6100161fae7248f
maximedb <maximedebruyn@gmail.com>
2017-10-02 21:39:54 +0200

    Work on Transformer model.

--- Model/Transformer/aia_model.py
+++ Model/Transformer/aia_model.py
@@ -9,7 +9,9 @@ import tensorflow as tf
 from aia_modules import char_convolution, positional_encoding, multihead_attention, feedforward
 
 def aiamodel(features, labels, mode, params):
-    
+    """
+    Tansformer model from "Attention is all you need"
+    """
     with tf.variable_scope('ModelParams'):
         batch_size = tf.shape(features['sequence'])[0]
         timesteps = tf.shape(features['sequence'])[1]
@@ -21,11 +23,11 @@ def aiamodel(features, labels, mode, params):
         kernels = params['kernels']
         kernel_features = params['kernel_features']
         ultimate_sequ_len = params['ultimate_sequ_len']
-        
+
     with tf.variable_scope('Convolution'):
         # Characters embeddings matrix. Basically each character id (int)
         # is associated a vector [char_embedding_size]
-        embeddings_c = tf.Variable(tf.random_uniform([params['char_vocab_size'], 
+        embeddings_c = tf.Variable(tf.random_uniform([params['char_vocab_size'],
                                    c_embed_s], -1.0, 1.0))
         # Embed every char id into their embedding. Will go from this dimension
         # [batch_size, max_sequence_length, max_word_size] to this dimension
@@ -36,20 +38,20 @@ def aiamodel(features, labels, mode, params):
                                         reuse=True)
         # Map the result to the 512 dimension
         conv_inputs = tf.layers.dense(conv_inputs, 512)
-        
+
         embedded_outputs = tf.nn.embedding_lookup(embeddings_c, labels['sequence'])
         # Apply a character convolution on the characters
         conv_outputs = char_convolution(embedded_outputs, kernels, kernel_features,
                                         reuse=True)
         # Map the result to the 512 dimension
         conv_outputs = tf.layers.dense(conv_outputs, 512)
-    
+
     with tf.variable_scope('Encoder'):
-        position_embeddings = positional_encoding(conv_outputs, 
+        position_embeddings = positional_encoding(conv_outputs,
                                                   ultimate_sequ_len, 512)
         encoder_inputs = position_embeddings + conv_outputs
         encoder_inputs = tf.layers.dropout(encoder_inputs, rate=dropout)
-        
+
         for i in range(params['num_blocks']):
             encoder_inputs = multihead_attention(queries=encoder_inputs,
                                                  keys=encoder_inputs,
@@ -58,26 +60,78 @@ def aiamodel(features, labels, mode, params):
                                                  dropout=dropout,
                                                  causality=False)
             encoder_inputs = feedforward(encoder_inputs)
-        
+
     with tf.variable_scope('Decoder'):
         # Decoder
-        if mode != tf.estimator.ModeKeys.PREDICT: 
-            # Training time so the decoder inputs are available, they wont be
-            # available at inference time.
-            # Should replace the first character of every sequence with the
-            # go character. output is of the shape [batc, seqlen, wordlen]
-            decoder_outputs = labels['sequence']
-            shape_deco = tf.shape(decoder_outputs)
-            go_char = tf.fill([shape_deco[0], 1, shape_deco[2]], params['go_char'])
-            decoder_inputs = tf.concat([go_char, decoder_outputs[:, :-1, :]], axis=2)
-            
-            embedded_outputs = tf.nn.embedding_lookup(embeddings_c, decoder_inputs)
-            # Apply a character convolution on the characters
-            decoder_inputs_e = char_convolution(embedded_outputs, kernels, kernel_features,
-                                            reuse=True)
-            # Map the result to the 512 dimension
-            decoder_inputs_e = tf.layers.dense(decoder_inputs, 512)
-            output_pos_embbed = positional_encoding(decoder_inputs_e, 
-                                                  ultimate_sequ_len, 512)
-            decoder_inputs += output_pos_embbed
-            decoder_inputs = tf.layers.dropout(decoder_inputs, rate=dropout)
+        # Training time so the decoder inputs are available, they wont be
+        # available at inference time.
+        # Should replace the first character of every sequence with the
+        # go character. output is of the shape [batc, seqlen, wordlen]
+        decoder_outputs = labels['sequence']
+        shape_deco = tf.shape(decoder_outputs)
+        go_char = tf.fill([shape_deco[0], 1, shape_deco[2]], params['go_char'])
+        decoder_inputs = tf.concat([go_char, decoder_outputs[:, :-1, :]], axis=2)
+
+        embedded_outputs = tf.nn.embedding_lookup(embeddings_c, decoder_inputs)
+        # Apply a character convolution on the characters
+        decoder_inputs_e = char_convolution(embedded_outputs, kernels, kernel_features,
+                                        reuse=True)
+        # Map the result to the 512 dimension
+        decoder_inputs_e = tf.layers.dense(decoder_inputs, 512)
+        output_pos_embbed = positional_encoding(decoder_inputs_e,
+                                              ultimate_sequ_len, 512)
+        # Add the positional embeddings
+        decoder_inputs += output_pos_embbed
+        # Add dropout on the decoder inputs.
+        decoder_inputs = tf.layers.dropout(decoder_inputs, rate=dropout)
+        decoder_outputs = decoder_inputs
+        for i in range(params['num_blocks']):
+            with tf.variable_scope('num_blocks_{}'.format(i))
+            # Multihead attention (self attention)
+            decoder_outputs = multihead_attention(queries=decoder_outputs,
+                                                  keys=decoder_inputs,
+                                                  num_units=512,
+                                                  num_heads=8,
+                                                  dropout=0.8,
+                                                  causality=True,
+                                                  scope="self_attention")
+
+            decoder_outputs = multihead_attention(queries=decoder_outputs,
+                                                  keys=encoder_inputs,
+                                                  num_units=512,
+                                                  num_heads=8,
+                                                  dropout=0.8,
+                                                  causality=False,
+                                                  scope="vanilla_attention")
+            decoder_outputs = feedforward(decoder_outputs)
+
+    # Final layer projection
+    with tf.variable_scope('final_layer'):
+        logits = tf.layers.dense(decoder_outputs, params['word_vocab_size'])
+        preds = tf.cast(tf.arg_max(self.logits, dimension=-1), tf.int32)
+        crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=decoder_o,
+                                                              logits=logits)
+        target_w = tf.sequence_mask(labels['sequence_length'], dtype=logits.dtype)
+        batch_size_32 = tf.cast(batch_size, tf.float32)
+        timesteps_32 = tf.cast(timesteps, tf.float32)
+        loss = (tf.reduce_sum(crossent * target_w) / (batch_size_32+timesteps_32))
+    # Train
+    with tf.variable_scope('train'):
+        if mode == tf.estimator.ModeKeys.TRAIN:
+            learning_rate = tf.train.exponential_decay(params['learning_rate'],
+                                               tf.train.get_global_step(),
+                                               params['decay_steps'],
+                                               0.96, staircase=True)
+            optimizer = tf.train.AdamOptimizer(learning_rate)
+            # Apply gradient clipping
+            gradients, variables = zip(*optimizer.compute_gradients(loss))
+            gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+            train_op = optimizer.apply_gradients(zip(gradients, variables),
+                                                 global_step=tf.train.get_global_step())
+            return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+    # Evaluation
+    with tf.variable_scope('eval')
+        eval_metric_ops = {"accuracy": tf.metrics.accuracy(labels=decoder_o,
+                                                   predictions=sample_id,
+                                                   weights=target_w)}
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
--- Model/Transformer/aia_modules.py
+++ Model/Transformer/aia_modules.py
@@ -8,155 +8,159 @@ Created on Tue Sep 26 20:07:39 2017
 import numpy as np
 import tensorflow as tf
 
-def conv2d(input_, output_dim, k_h, k_w, name="conv2d"):
+def conv2d(input_, output_dim, k_h, k_w, name="conv2d", reuse=None):
     # cnn_inputs, kernel_feature_size, 1, kernel_size
-    with tf.variable_scope(name):
+    with tf.variable_scope(name, reuse=reuse):
         w = tf.get_variable('w', [k_h, k_w, input_.get_shape()[-1], output_dim])
         b = tf.get_variable('b', [output_dim])
-    return tf.nn.conv2d(input_, w, strides=[1, 1, 1, 1], padding='VALID') + b
+        return tf.nn.conv2d(input_, w, strides=[1, 1, 1, 1], padding='VALID') + b
 
-def char_convolution(inputs, kernels, kernel_features, reuse=None):
-    # Given an input of shape [batch, s_length, w_length, embed_dim]
-    # performs a 1D convolutions with multiple kernel sizes.
-    s_length = tf.shape(inputs)[1]
-    w_length = tf.shape(inputs)[2]
-    e_length = tf.shape(inputs)[3]
-    cnn_inputs = tf.reshape(inputs, [batch_size*s_length, w_length, e_length])
-    # Expand the second dimension for convolution purposes
-    cnn_inputs = tf.expand_dims(cnn_inputs, 1)
-    # Layer to hold all of the convolution results
-    layers = []
-    # For each kernel, tuple of [kernel size, num filters]
-    for kernel_size, kernel_feature_size in zip(kernels, kernel_features):
-        # Apply the convolution on all of the inputs for this kernal
-        conv = conv2d(cnn_inputs, kernel_feature_size, 1, kernel_size, 
-                      name="kernel_%d" % kernel_size, reuse=reuse)
-        # Take the max pooling
-        pool = tf.reduce_max(tf.tanh(conv), 2, keep_dims=True)
-        # Append the squeezed version
-        layers.append(tf.squeeze(pool, [1, 2]))
-    # Concat the results along the second axis to give words embeddings
-    cnn_output = tf.concat(layers, 1)
-    # Reshape it to match sequence length
-    cnn_output = tf.reshape(cnn_output, [batch_size, timesteps, sum(kernel_features)])
-    return cnn_output
+def char_convolution(inputs, kernels, kernel_features, scope="char_convolution", reuse=None):
+    with tf.variable_scope(scope, reuse=reuse):
+        # Given an input of shape [batch, s_length, w_length, embed_dim]
+        # performs a 1D convolutions with multiple kernel sizes.
+        s_length = tf.shape(inputs)[1]
+        w_length = tf.shape(inputs)[2]
+        e_length = tf.shape(inputs)[3]
+        cnn_inputs = tf.reshape(inputs, [batch_size*s_length, w_length, e_length])
+        # Expand the second dimension for convolution purposes
+        cnn_inputs = tf.expand_dims(cnn_inputs, 1)
+        # Layer to hold all of the convolution results
+        layers = []
+        # For each kernel, tuple of [kernel size, num filters]
+        for kernel_size, kernel_feature_size in zip(kernels, kernel_features):
+            # Apply the convolution on all of the inputs for this kernal
+            conv = conv2d(cnn_inputs, kernel_feature_size, 1, kernel_size,
+                          name="kernel_%d" % kernel_size, reuse=reuse)
+            # Take the max pooling
+            pool = tf.reduce_max(tf.tanh(conv), 2, keep_dims=True)
+            # Append the squeezed version
+            layers.append(tf.squeeze(pool, [1, 2]))
+        # Concat the results along the second axis to give words embeddings
+        cnn_output = tf.concat(layers, 1)
+        # Reshape it to match sequence length
+        cnn_output = tf.reshape(cnn_output, [batch_size, timesteps, sum(kernel_features)])
+        return cnn_output
 
-def positional_encoding(inputs, max_len, num_units):
+def positional_encoding(inputs, max_len, num_units, scope="pos_embedding", reuse=None):
     """Returns the sinusoidal encodings matrix
     inputs: matrix with the inputs
     max_len: the max timesteps of the model
     num_units: the dimension to encode every timestep in (512)
     """
-    # Variable like [1, 2, ... max_len]*batch_size
-    input_one = tf.tile(tf.expand_dims(tf.range(tf.shape(inputs)[1]), 0), 
-                                                [tf.shape(inputs)[0], 1])
-    position_enc = np.array([
-        [pos / np.power(10000, 2*i/num_units) for i in range(num_units)]
-        for pos in range(max_len)])
+    with tf.variable_scope(scope, reuse=reuse)
+        # Variable like [1, 2, ... max_len]*batch_size
+        input_one = tf.tile(tf.expand_dims(tf.range(tf.shape(inputs)[1]), 0),
+                                                    [tf.shape(inputs)[0], 1])
+        position_enc = np.array([
+            [pos / np.power(10000, 2*i/num_units) for i in range(num_units)]
+            for pos in range(max_len)])
 
-    position_enc[:, 0::2] = np.sin(position_enc[1:, 0::2]) # dim 2i
-    position_enc[:, 1::2] = np.cos(position_enc[1:, 1::2]) # dim 2i+1
-    
-    lookup_table = tf.convert_to_tensor(position_enc)
-    outputs = tf.nn.embedding_lookup(lookup_table, input_one)
-    outputs = outputs * math.sqrt(num_units)
-    return outputs
+        position_enc[:, 0::2] = np.sin(position_enc[1:, 0::2]) # dim 2i
+        position_enc[:, 1::2] = np.cos(position_enc[1:, 1::2]) # dim 2i+1
 
-def embedding(inputs, vocab_size, num_units):
-    # Variable that holds the mapping between the ids and their representation
-    lookup_table = tf.get_variable('lookup_table',
-                       dtype=tf.float32,
-                       shape=[vocab_size, num_units],
-                       initializer=tf.contrib.layers.xavier_initializer())
-    # Change the first row with a tensor full of zeros (PAD id)
-    lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
-                                      lookup_table[1:, :]), 0)
-    # Return the embeddings
-    outputs = tf.nn.embedding_lookup(lookup_table, inputs)
-    # Scale it with the square root of num_units 
-    return outputs * (num_units ** 0.5) 
+        lookup_table = tf.convert_to_tensor(position_enc)
+        outputs = tf.nn.embedding_lookup(lookup_table, input_one)
+        outputs = outputs * math.sqrt(num_units)
+        return outputs
 
-def multihead_attention(queries, keys, num_heads=8, dropout=0,
-                        causality=False):
-    
-    num_units = queries.get_shape()[-1]
-    projection_dim = num_units / num_heads
-    # Variable to store the intermediate multi-head attention
-    intermediate_outputs = []
-    # For every head
-    for i in range(num_heads):
-        # Project the queries, keys and values on projection_dim
-        Q = tf.layers.dense(queries, projection_dim)
-        K = tf.layers.dense(keys, projection_dim)
-        V = tf.layers.dense(keys, projection_dim) 
-        
-        # Multiply the query with the keys to get the attention scores
-        scores = tf.matmul(Q, tf.transpose(K))
-        
-        # Scale the result
-        scaled_scores = scores / (K.get_shape()[-1] ** 0.5)
-        
-        # Mask the scores that are 'illegal'
-        # Causality = Future blinding
-        if causality:
-            # Create a vector of ones that has the same size as the self
-            # projection for one batch
-            diag_vals = tf.ones_like(scaled_scores[0, :, :]) # (T_q, T_k)
-            # Create a lower triangular matrix with it.
-            # The first row will be equal to one only for the first item
-            # So all the other attention will be set to minus infinity after
-            tril = tf.contrib.linalg.LinearOperatorTriL(diag_vals).to_dense() 
-            # Expand the mask for the batch_size
-            masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(scaled_scores)[0], 1, 1]) 
-            # Padding is the value of initinity that will be used when the 
-            # attentionw will be illegal
-            paddings = tf.ones_like(masks)*(-2**32+1)
-            # Wheneve the mask equal one, replace the score value with the value
-            # of the padding (-inf)
-            scaled_scores = tf.where(tf.equal(masks, 0), paddings, scaled_scores) # (h*N, T_q, T_k)
-        
-        # Perform the softmax
-        softmax = tf.nn.softmax(scaled_scores)
-        # Apply dropout
-        dropout = tf.layers.dropout(softmax, rate=dropout)
-        # Compute the attention (finally)
-        outputs = tf.matmul(dropout, V)
-        # Append the result
-        intermediate_outputs.append(outputs)
-    # Out of the loop
-    # Concat the intermediate results
-    concat = tf.concat(intermediate_outputs, axis=2)
-    # Apply a last linear projection
-    linear = tf.layers.dense(concat, num_units, activation=tf.nn.relu)
-    # Residual connection (Add)
-    outputs = queries + linear
-    # Normalize outputs (Norm)
-    outputs = tf.contrib.layers.layer_norm(outputs)
-    return linear
+def embedding(inputs, vocab_size, num_units, scope="embedding", reuse=None):
+    with tf.variable_scope(scope, reuse=True)
+        # Variable that holds the mapping between the ids and their representation
+        lookup_table = tf.get_variable('lookup_table',
+                           dtype=tf.float32,
+                           shape=[vocab_size, num_units],
+                           initializer=tf.contrib.layers.xavier_initializer())
+        # Change the first row with a tensor full of zeros (PAD id)
+        lookup_table = tf.concat((tf.zeros(shape=[1, num_units]),
+                                          lookup_table[1:, :]), 0)
+        # Return the embeddings
+        outputs = tf.nn.embedding_lookup(lookup_table, inputs)
+        # Scale it with the square root of num_units
+        return outputs * (num_units ** 0.5)
 
-def feed_forward(inputs):
+def multihead_attention(queries, keys, num_units=None, num_heads=8, dropout=0,
+                        causality=False, scope="multihead_attention",
+                        reuse=None):
+    with tf.variable_scope(scope, reuse=reuse)
+        num_units = queries.get_shape()[-1]
+        projection_dim = num_units / num_heads
+        # Variable to store the intermediate multi-head attention
+        intermediate_outputs = []
+        # For every head
+        for i in range(num_heads):
+            # Project the queries, keys and values on projection_dim
+            Q = tf.layers.dense(queries, projection_dim)
+            K = tf.layers.dense(keys, projection_dim)
+            V = tf.layers.dense(keys, projection_dim)
+
+            # Multiply the query with the keys to get the attention scores
+            scores = tf.matmul(Q, tf.transpose(K))
+
+            # Scale the result
+            scaled_scores = scores / (K.get_shape()[-1] ** 0.5)
+
+            # Mask the scores that are 'illegal'
+            # Causality = Future blinding
+            if causality:
+                # Create a vector of ones that has the same size as the self
+                # projection for one batch
+                diag_vals = tf.ones_like(scaled_scores[0, :, :]) # (T_q, T_k)
+                # Create a lower triangular matrix with it.
+                # The first row will be equal to one only for the first item
+                # So all the other attention will be set to minus infinity after
+                tril = tf.contrib.linalg.LinearOperatorTriL(diag_vals).to_dense()
+                # Expand the mask for the batch_size
+                masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(scaled_scores)[0], 1, 1])
+                # Padding is the value of initinity that will be used when the
+                # attentionw will be illegal
+                paddings = tf.ones_like(masks)*(-2**32+1)
+                # Wheneve the mask equal one, replace the score value with the value
+                # of the padding (-inf)
+                scaled_scores = tf.where(tf.equal(masks, 0), paddings, scaled_scores) # (h*N, T_q, T_k)
+
+            # Perform the softmax
+            softmax = tf.nn.softmax(scaled_scores)
+            # Apply dropout
+            dropout = tf.layers.dropout(softmax, rate=dropout)
+            # Compute the attention (finally)
+            outputs = tf.matmul(dropout, V)
+            # Append the result
+            intermediate_outputs.append(outputs)
+        # Out of the loop
+        # Concat the intermediate results
+        concat = tf.concat(intermediate_outputs, axis=2)
+        # Apply a last linear projection
+        linear = tf.layers.dense(concat, num_units, activation=tf.nn.relu)
+        # Residual connection (Add)
+        outputs = queries + linear
+        # Normalize outputs (Norm)
+        outputs = tf.contrib.layers.layer_norm(outputs)
+        return linear
+
+def feed_forward(inputs, scope="multihead_attention", reuse=None):
     # This layer needs to be seen as simple dense layer that is applied
     # element per element (i.e., word per word), just like ... a 1D convolution
     # on the inputs
     # This layer has two part. First one a element dense layer of size 22048
-    # with a relu activation function 
-    params = {"inputs": inputs, "filters": 2048, "kernel_size": 1,
-                  "activation": tf.nn.relu, "use_bias": True}
-    outputs = tf.layers.conv1d(**params)
-    # And a second layer element wise dense layer that project the output
-    # to the num_units of 512
-    params = {"inputs": outputs, "filters": 512, "kernel_size": 1,
-              "activation": None, "use_bias": True}
-    outputs = tf.layers.conv1d(**params)
-    # Residual connection (Add)
-    outputs += inputs
-    # Normalize outputs (Norm)
-    outputs = tf.contrib.layers.layer_norm(outputs)
-    return outputs
-    
+    # with a relu activation function
+    with tf.variable_scope(scope, reuse=reuse):
+        params = {"inputs": inputs, "filters": 2048, "kernel_size": 1,
+                      "activation": tf.nn.relu, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+        # And a second layer element wise dense layer that project the output
+        # to the num_units of 512
+        params = {"inputs": outputs, "filters": 512, "kernel_size": 1,
+                  "activation": None, "use_bias": True}
+        outputs = tf.layers.conv1d(**params)
+        # Residual connection (Add)
+        outputs += inputs
+        # Normalize outputs (Norm)
+        outputs = tf.contrib.layers.layer_norm(outputs)
+        return outputs
+
 def label_smoothing(inputs, epsilon=0.1):
     K = inputs.get_shape()[-1]
     return ((1-epsilon) * inputs) + (epsilon / K)
-    
+
 def normalize(inputs, epsilon=1e-8):
-    
