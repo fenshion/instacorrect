0e9f993eb615886cd6758a7d615511e7b7103826
maximedb <maximedebruyn@gmail.com>
2017-10-22 13:51:42 +0200

    web server

--- Server/web/project/__init__.py
+++ Server/web/project/__init__.py
@@ -6,21 +6,19 @@ Created on Fri Sep 15 08:58:44 2017
 
 Simple Flask app that serves two purposes:
 - render the index.html
-- take request for corrections, send them to the tf server and give the transformed
+- take request for corrections, send them to the tf server and give the
   result back to the browser.
 """
 from __future__ import print_function
-import sys
 from flask import Flask, request, jsonify, render_template
-import json
-import os
 import nltk
 nltk.download('punkt')
 nltk.download('perluniprops')
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import sent_tokenize
 from correct import correct_sentence
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -29,19 +27,21 @@ def index():
     """
     return render_template('index.html')
 
-# curl -H "Content-Type: application/json" -X POST -d '{"sentence":"this is my sentence"}' http://0.0.0.0/api/is_correct
+
+# curl -H "Content-Type: application/json" -X POST -d
+# '{"sentence":"this is my sentence"}' http://0.0.0.0/api/is_correct
 @app.route('/api/is_correct', methods=['POST'])
 def is_correct():
     """
-    API endpoint to correct a given sentence. Will first encode the sentence into
-    then send it to the model, it will then compare the results.
+    API endpoint to correct a given sentence. Will first encode the sentence
+    into then send it to the model, it will then compare the results.
     """
     data = request.get_json()
     # Check that the data received contains a sentence
     if 'sentence' not in data:
         return "Please give a sentence to correct", 400
-    text = u""+data['sentence'] # Just to be sure it is a unicode string.
-    #Cut the text into (possible) multiple sentences.
+    text = u""+data['sentence']  # Just to be sure it is a unicode string.
+    # Cut the text into (possible) multiple sentences.
     sentences = sent_tokenize(text)
     if len(sentences) > 10 | len(text) > 1000:
         return "Please reduce the size of the text", 400
@@ -49,7 +49,6 @@ def is_correct():
     for i, sentence in enumerate(sentences):
         tmp_response = correct_sentence(sentence)
         corrected_sentences.append(tmp_response)
-        #`response = {'correct': correct_probability, 'uncorrect': 1-correct_probability}
     contacted_sentences = " ".join(corrected_sentences)
     return jsonify({"sentence": contacted_sentences})
 
--- Server/web/project/correct.py
+++ Server/web/project/correct.py
@@ -1,33 +1,35 @@
 from __future__ import print_function
-import sys
 from grpc.beta import implementations
 from tensorflow_serving.apis import predict_pb2
 from tensorflow_serving.apis import prediction_service_pb2
-from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.tokenize import word_tokenize
 from nltk.tokenize.moses import MosesDetokenizer
 import tensorflow as tf
 import io
-import numpy as np
 import json
-import os
+
 # Load the character vocabulary
-with io.open("/app/data/vocab/char_vocab_dict.json", 'r', encoding='utf8') as fin:
-        char_vocab=json.loads(fin.readline())
+with io.open("/app/data/vocab/char_vocab_dict.json",
+             'r', encoding='utf8') as fin:
+        char_vocab = json.loads(fin.readline())
 
 # Load the word vocabulary
-with io.open("/app/data/vocab/words_vocab_dict.json", 'r', encoding='utf8') as fin:
-        word_vocab=json.loads(fin.readline())
+with io.open("/app/data/vocab/words_vocab_dict.json",
+             'r', encoding='utf8') as fin:
+        word_vocab = json.loads(fin.readline())
 
 # Load the rverse word vocabulary
-with io.open("/app/data/vocab/words_vocab_reve.json", 'r', encoding='utf8') as fin:
-        reve_word_vocab=json.loads(fin.readline())
+with io.open("/app/data/vocab/words_vocab_reve.json",
+             'r', encoding='utf8') as fin:
+        reve_word_vocab = json.loads(fin.readline())
 
 detokenizer = MosesDetokenizer()
-eos = word_vocab['|EOS|']
+eos = word_vocab['EOS']
 vocab_set = set(word_vocab.keys())
 
+
 def correct_sentence(sentence):
-    """Ask the server to know if the given sentence is correct"""
+    """Ask the server to correct this sentence"""
     host = "tfserver"
     port = 9000
     model_name = "instacorrect"
@@ -40,47 +42,42 @@ def correct_sentence(sentence):
     request.inputs['examples'].CopyFrom(serialized_examples)
     result = stub.Predict(request, 5.0)  # 5 seconds
     corrected_sentence = result.outputs['sequence'].int_val
-    decoded_sentence = decode_sentence(corrected_sentence, sentence)
+    decoded_sentence = decode_sentence(corrected_sentence)
     return decoded_sentence
 
-def decode_sentence(sample_ids, initial_sentence):
+
+def decode_sentence(sample_ids):
     decoded = [reve_word_vocab[str(ids)] for ids in sample_ids if ids != eos]
-    initial_sentence = word_tokenize(initial_sentence)
-    unks = {str(i): word for i, word in enumerate(initial_sentence) if word not in vocab_set}
-    for i, word in enumerate(decoded):
-        if word == '|UNK|':
-            decoded[i] = unks.get(str(i), '|UNK|')
     decoded = detokenizer.detokenize(decoded, return_str=True)
     return decoded.capitalize()
 
+
 def create_example(line, vocab):
     """Given a string and a label (and a vocab dict), returns a tf.Example"""
-    sequence, sequence_length, max_word_length = encode_line_charwise(line, vocab)
-    example = tf.train.Example(features=tf.train.Features(feature={
-            'mistake_sequence': _int64_feature(sequence),
-            'mistake_sequence_length': _int64_feature([sequence_length]),
-            'mistake_max_word_length':_int64_feature([max_word_length])}))
+    seq, seq_l, max_word = encode_line_charwise(line, vocab)
+    features = {'input_sequence': _int64_feature(seq),
+                'input_sequence_length': _int64_feature([seq_l]),
+                'input_sequence_maxword': _int64_feature([max_word])}
+    example = tf.train.Example(features=tf.train.Features(feature=features))
     return example
 
-def encode_line(sentence):
-    """Encode a line word wise"""
-    words = word_tokenize(sentence)
-    return [word_vocab.get(word, word_vocab['|UNK|']) for word in words]
 
 def encode_line_charwise(line, vocab):
     """Given a string will encode it into the right tf.example format"""
     # Encode the string into their vocab representation
     splited = word_tokenize(line)
     sequence_length = len(splited)
-    max_word_length = max([len(word) for word in splited])
+    max_word_length = max([len(word)+2 for word in splited])
     # Should have a one array of int.
     sequence = []
     pad_char = vocab.get('|PAD|')
     for word in splited:
-        word_encoded = [vocab.get(char, vocab['|UNK|']) for char in word]
-        word_encoded += [pad_char]*(max_word_length - len(word))
+        word_encoded = [vocab.get('{')] + [vocab.get(char, vocab['|UNK|'])
+                                           for char in word] + [vocab.get('}')]
+        word_encoded += [pad_char]*(max_word_length - len(word_encoded))
         sequence.extend(word_encoded)
     return sequence, sequence_length, max_word_length
+
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
